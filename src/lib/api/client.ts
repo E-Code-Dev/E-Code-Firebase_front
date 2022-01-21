8b31c6fcad3e2ba3497eb65647376d847aedb5e0
makotty <hal.m.90215@gmail.com>
2022-01-21 23:03:00 +0900

    開発環境用のURLの追加

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -9,6 +9,7 @@ const options = {
 const client = applyCaseMiddleware(
   axios.create({
     baseURL: 'https://e-code-back.herokuapp.com/api/v1'
+    // baseURL: 'http://localhost:8080/api/v1'
   }),
   options
 )
