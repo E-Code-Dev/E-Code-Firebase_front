b4e06604f4060ecc6e335666c4ab06a172d72392
makotty <hal.m.90215@gmail.com>
2022-03-05 21:42:04 +0900

    アカウント選択画面のスタイリング調整

--- src/assets/styles/pages/AccountSelectionStyled.ts
+++ src/assets/styles/pages/AccountSelectionStyled.ts
@@ -2,11 +2,10 @@ import styled from '@emotion/styled'
 import { Box } from '@mui/material'
 
 export const ECodeIconBox = styled(Box)`
-  width: 80%;
   margin: 0 auto;
-  padding: 64px;
+  padding: 128px;
   @media screen and (max-width: 600px) {
-    padding: 48px;
+    padding: 64px;
   }
 `
 
